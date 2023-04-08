b22862412a331e55f8b5b0cd77db953d7ec1dcf4
inmyblue <98289614+inmyblue@users.noreply.github.com>
2023-04-08 09:51:45 +0900

    bug fix : decorator 누락

--- src/users/entities/social.entity.ts
+++ src/users/entities/social.entity.ts
@@ -1,11 +1,12 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { BasicEntity } from 'src/common/entities/basic.entity';
-import { Column, OneToOne } from 'typeorm';
+import { Column, Entity, OneToOne } from 'typeorm';
 import { User } from './user.entity';
 import { SocialType } from '../enums/social_type.enum';
 
 @InputType('SocialInputType', { isAbstract: true })
 @ObjectType()
+@Entity()
 export class Social extends BasicEntity {
   @Field((type) => SocialType, { description: '소셜로그인 타입' })
   @Column({ type: 'enum', enum: SocialType })
